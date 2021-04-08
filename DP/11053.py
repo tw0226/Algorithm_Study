b5cb7232886892ab04b383278ae86525de5f201a
DESKTOP-J1A707F\김태우 <kimraewoo99@gmail.com>
2021-04-08 18:42:37 +0900

    Algorithm update

---
+++ DP/11053.py
@@ -0,0 +1,10 @@
+n = int(input())
+A = list(map(int, input().split()))
+dp = [1] * n
+# dp.append(A[0])
+
+for i in range(n):
+    for j in range(i):
+        if A[i]>A[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+print(max(dp))
