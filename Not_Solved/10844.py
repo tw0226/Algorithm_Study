0c7f5fcc2e16d55d282f7af1b8e8f6387a691e19
kimtaewoo <kimraewoo99@gmail.com>
2021-04-14 19:13:00 +0900

    10844 clear - try again

--- Not_Solved/10844.py
+++ Not_Solved/10844.py
@@ -1,16 +1,15 @@
 n = int(input())
-dp = [0 for i in range(n+2)]
-for i in range(n+2):
+dp = [0 for i in range(n+1)]
+for i in range(n+1):
     dp[i] = [0 for i in range(10)]
 dp[1] = [0] + [1 for i in range(9)]
-# dp[2] =  10 12 21 23 32 34 43 45 54 56 65 67 76 78 87 89 98
-# dp[3] = 987 989   # 876 878 890 898
-# dp[4] = 9876 9878 9898
-# dp[5] = 98765 98767 98787 98789 98987 98989
-for i in range(1, n+1):
-    for j in range(9, 0, -1):
-        if i+j>=10 or i>j:
-            dp[i+1][j] = dp[i][j]+1
+for i in range(2, n+1):
+    for j in range(10):
+        if j==0:
+            dp[i][j] = dp[i-1][1]
+        elif j==9:
+            dp[i][j] = dp[i-1][8]
         else:
-            dp[i+1][j] = dp[i][j]*2
-    print(dp[i], sum(dp[i]))
+            dp[i][j]=dp[i-1][j-1] + dp[i-1][j+1]
+print(sum(dp[n])%1000000000)
+# Need to Try again
