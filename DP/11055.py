d41bab1743940e440ef64d4ebc3d08f7ccdf11fc
kimtaewoo <kimraewoo99@gmail.com>
2021-04-19 16:02:29 +0900

    11055 solve + 다시 풀어보기

---
+++ DP/11055.py
@@ -0,0 +1,11 @@
+n = int(input())
+a = list(map(int, input().split()))
+dp = [x for x in a]
+# dp[0] = [a[0]] * (n+1)
+# print(dp[0])
+for i in range(n):
+    for j in range(i):
+        if a[i]>a[j]:
+            dp[i] = max(dp[i], dp[j]+a[i])
+        print(dp)
+
--- DP/9251.py
+++ DP/9251.py
@@ -3,7 +3,6 @@ y = input()
 
 dp = [[0] *(len(y)+1) for _ in range(len(x)+1)]
 
-
 for i in range(1, len(x)+1):
 	for j in range(1, len(y)+1):
 		if x[i-1] == y[j-1]:
