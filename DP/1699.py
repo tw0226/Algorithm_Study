762b05da2e39d76706f3c922508a1bdcf84beef5
DESKTOP-J1A707F\김태우 <kimraewoo99@gmail.com>
2021-04-26 23:17:07 +0900

    1699 S3 Helped.. T_T

--- DP/1699.py
+++ DP/1699.py
@@ -1,13 +1,13 @@
 n = int(input())
-count=0
-while n>0:
-    k = int(n**0.5)
-    if n==k*k:
-        count+=1
-        break
-    else:
-        n-=k*k
-        count+=1
-print(count)
+dp = [0 for i in range(n + 1)]
+square = [i * i for i in range(1, 317)]
+for i in range(1, n + 1):
+    s = []
+    for j in square:
+        if j > i:
+            break
+        s.append(dp[i - j])
+    dp[i] = min(s) + 1
+print(dp[n])
 
 # 못풀어따
