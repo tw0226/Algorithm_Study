7f559964a589fe5043b023b8f61f85ed0ecde0bc
kimtaewoo <kimraewoo99@gmail.com>
2021-07-16 01:45:50 +0900

    DP 11660 S1 Solved by myself

--- DP/11660.py
+++ DP/11660.py
@@ -1,14 +1,13 @@
-n, m = map(int, input().split())
-a = [0 for i in range(n)]
-for i in range(n):
-    a[i] = list(map(int, input().split()))
+N, M = map(int, (input().split()))
+arr = [0 for i in range(N)]
+dp = [[0 for i in range(N+1)] for j in range(N+1)]
 
-dp = [[0 for i in range(n + 1)] for i in range(n + 1)]
+for i in range(N):
+    arr[i] = list(map(int, input().split()))
+for i in range(N):
+    for j in range(N):
+        dp[i + 1][j + 1] = (dp[i][j + 1] + dp[i + 1][j] - dp[i][j]) + arr[i][j]
 
-m_list = []
-for i in range(n):
-    for j in range(n):
-        dp[i + 1][j + 1] = (dp[i][j + 1] + dp[i + 1][j] - dp[i][j]) + a[i][j]
-for i in range(m):
+for i in range(M):
     x1, y1, x2, y2 = map(int, input().split())
-    print(dp[x2][y2] - (dp[x1 - 1][y2] + dp[x2][y1 - 1] - dp[x1 - 1][y1 - 1]))
+    print(dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1])
