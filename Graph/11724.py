20b0c0078d0a69d56364ff88b14b9ea362a21213
TW <kimreawoo99@gmail.com>
2022-03-29 01:14:30 +0900

    update 11724

--- Graph/11724.py
+++ Graph/11724.py
@@ -1,23 +1,21 @@
+import sys
+sys.setrecursionlimit(10**6)
 N, M = map(int, input().split())
 graph = [[0 for _ in range(N+1)] for _ in range(N+1)]
 visited = [0 for _ in range(N+1)]
 for i in range(M):
-    a, b = map(int, input().split())
-    graph[a][b] = 1
+    a, b = map(int,sys.stdin.readline().split())
+    graph[a][b] = graph[b][a] = 1
 
-def dfs(stack):
-    while stack:
-        a = stack.pop()
-        # print(a)
-        visited[a] = 1
-        for i in range(1, N+1):
-            if graph[a][i] != 0 and not visited[i]:
-                graph[a][i] = 0
-                dfs([i])
+def dfs(a):
+    visited[a] = 1
+    for i in range(1, N+1):
+        if graph[a][i] != 0 and not visited[i]:
+            dfs(i)
 
 count = 0
 for i in range(1, N+1):
     if not visited[i]:
-        dfs([i])
+        dfs(i)
         count += 1
 print(count)
