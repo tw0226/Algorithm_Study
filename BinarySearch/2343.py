370ddeb05fc9cc837a1da8012fdb04e6610fee63
tw0226 <kimraewoo99@gmail.com>
2025-03-07 02:01:28 +0900

    2343 solved by help

--- BinarySearch/2343.py
+++ BinarySearch/2343.py
@@ -1,2 +1,17 @@
 N, M = map(int, input().split())
-lec = list(map, int(input().split()))
+lectures = list(map(int, input().split(' ')))
+start, end = max(lectures), sum(lectures)
+while start <= end:
+    mid = (start + end) // 2
+    total_mins = 0
+    count = 1
+    for lec in lectures:
+        if total_mins + lec > mid:
+            count += 1
+            total_mins = 0
+        total_mins += lec
+    if count <= M:
+        end = mid - 1
+    else:
+        start = mid + 1
+print(start)
