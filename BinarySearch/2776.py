bf12680457010520ab1f4d021cbf49c268232df1
tw0226 <kimraewoo99@gmail.com>
2025-03-09 01:58:21 +0900

    solve 2776

--- BinarySearch/2776.py
+++ BinarySearch/2776.py
@@ -1,14 +1,22 @@
 T = int(input())
-D1 = int(input())
-num_d1 = list(map(int, input().split()))
-D2 = int(input())
 note_dict = {}
-num_d2 = list(map(int, input().split()))
-for num in num_d2:
-    note_dict[num] = 1
 
-for dict in note_dict:
-    if dict in num_d1:
-        print(1)
-    else:
-        print(0)
+for t in range(T):
+    D1 = int(input())
+    note1 = list(map(int, input().split()))
+    D2 = int(input())
+    note2 = list(map(int, input().split()))
+    note1 = sorted(note1)
+    for num in note2:
+        start, end = 0, len(note1)-1
+        exist = 0
+        while start <= end:
+            mid = (start + end) // 2
+            if note1[mid] == num:
+                exist = 1
+                break
+            elif note1[mid] < num:
+                start = mid +1
+            else:
+                end = mid -1
+        print(exist)
