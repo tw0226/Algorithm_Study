ec1fa858f20257e4daab10a3f7a82be00b335d68
tw0226 <kimraewoo99@gmail.com>
2025-03-20 23:06:11 +0900

    solve 12015

---
+++ BinarySearch/12015.py
@@ -0,0 +1,12 @@
+import bisect
+N = int(input())
+A = list(map(int, input().split()))
+curr = [A[0]]
+for i in range(N):
+    if A[i] > curr[-1]:
+        curr.append(A[i])
+    else:
+        left = bisect.bisect_left(curr, A[i])
+        curr[left] = A[i]
+    print(curr)
+print(len(curr))
