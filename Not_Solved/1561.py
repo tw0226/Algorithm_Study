9ccd92ee75644f171ae4b14d345ebb2f620d678f
tw0226 <kimraewoo99@gmail.com>
2025-03-24 23:59:40 +0900

    1561 resolve

--- Not_Solved/1561.py
+++ Not_Solved/1561.py
@@ -1,16 +1,33 @@
 # 어떻게 풀까?
-# 1) DP 같은 곳에다가 누적합으로 쌓게 만들고, 이분탐색으로 누적합이 N이 되는 지점 찾기
+# 1) DP 같은 곳에다가 누적합으로 쌓게 만들고, 이분탐색으로 누적합이 N이 되는 지점 찾기 ?? => 문제 이해가 잘 안됌
 
 import sys
 n, m =  map(int, input().split())
 p = list(map(int, sys.stdin.readline().split()))
-dp = [0 for i in range(m+1)]
 
-for i in p:
-    for j in range(1, 10000, i):
-        dp[j] += dp[j-i] + 1
-
-start, end = 1, 2e9
-while start < end:
-    mid = (start+end)//2
-    
+if n<m:
+    print(n)
+else:
+    start, end = 1, 2e9
+    total = 0
+    while start <= end:
+        mid = (start+end)//2
+        count = m
+        for i in range(m):
+            count += mid//p[i]
+        if count >= n:
+            total = mid
+            end = mid -1
+        else:
+            start = mid +1
+        
+        count = m
+        for i in range(m):
+            count += (total-1)//p[i]
+        
+        for i in range(m):
+            if total % p[i] == 0:
+                count += 1
+            if count == n:
+                print(i+1)
+                break
