d5a70aefb88f7552ef4aa4d60615d00cb56a0bb8
tw0226 <kimraewoo99@gmail.com>
2025-04-19 23:48:17 +0900

    solving 1800..

---
+++ Not_Solved/1800.py
@@ -0,0 +1,38 @@
+# 다익스트라는 구현했는데 뭔가 잘못된 것 같다..
+import heapq
+n, p, k = map(int, input().split())
+net = [[] for i in range(n+1)]
+for i in range(p):
+    a,b,c = map(int, input().split())
+    net[a].append((c, b))
+    net[b].append((c, a))
+
+node_cost = [1e9 for i in range(n+1)]
+visited = [0 for i in range(n+1)]
+costs = []
+node = []
+heapq.heappush(node, (0, 1))
+
+while node:
+    curr_cost, curr_node = heapq.heappop(node)
+    visited[curr_node] = True
+    for next_cost, next_node in net[curr_node]:
+        if visited[next_node]:
+            continue
+        min_cost = curr_cost + next_cost
+        print("!", curr_node, next_node, min_cost)
+        if not visited[next_node] and min_cost < node_cost[next_node]:
+            node_cost[next_node] = min_cost
+            heapq.heappush(node, (min_cost, next_node))
+            costs.append(next_cost)
+
+
+print(costs)
+costs = sorted(costs, reverse=True)
+print(visited)
+print(node_cost)
+print(costs)
+if not visited[n]:
+    print(-1)
+else:
+    print(costs[k])
